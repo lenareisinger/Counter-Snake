0eee28f9459040587402e346ee7349ec88039708
Tomas <langer.tomas@yahoo.com>
2013-02-07 13:45:43 +0000

    Tomas

--- snake.java
+++ snake.java
@@ -1,7 +1,8 @@
 public class snake {
 
     public static void main( String[] args ) {
-        System.out.println("Hello World");
+        System.out.println("Hello Guys, I just wanted to try it xD");
+        System.out.println("Also, Ill try to do something before the tutorial so we can be producitve xD");
     }
 
 }
