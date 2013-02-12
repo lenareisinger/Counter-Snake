92c01fd5c999eacc079d995244fb4493ae934239
Tomas <langer.tomas@yahoo.com>
2013-02-12 18:27:00 +0000

    Tomas
    Here it is... The food does not move and you can go through walls but
    check out the pretty background xD

---
+++ Snake/src/SnakeMain.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+
+public class SnakeMain {
+	JFrame      mainWindow;
+	SnakePanel helloPanel;
+	
+	// Create a new window, and put helloPanel inside it.
+	public SnakeMain() {
+		mainWindow = new JFrame("Bouncing Ball");
+		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		helloPanel = new SnakePanel();
+		mainWindow.getContentPane().add(helloPanel);
+        
+		mainWindow.pack();
+		mainWindow.setVisible(true);
+	}
+    
+	private static void createAndShowGUI() {
+		// Make sure we have nice window decorations.
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		// Create an actual instance of HelloBounceApp
+		new SnakeMain();		
+	}
+
+	public static void main(String[] args) {
+		// Schedule a job for the event-dispatching thread:
+		// creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
+}
---
+++ Snake/src/SnakePanel.java
@@ -0,0 +1,210 @@
+import java.awt.*;
+
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class SnakePanel extends JComponent 
+                         implements ComponentListener, KeyListener, Runnable {
+	
+	final static long serialVersionUID = 0;  // kill warning
+	
+    // constants
+    final static BasicStroke stroke = new BasicStroke(2.0f);
+      
+    // fields
+    float xsize,ysize;  // size of window
+    float[] xp = new float[100];	// x position of snake
+    float[] yp = new float[100];    // y position of snake
+    int snakeSize = 4;
+    float xlen,ylen;    // size of ball
+    float dx,dy;        // current speed + direction of ball 
+    boolean start = true;
+    BufferedImage background;
+    
+    int    delay;       // delay between frames in milliseconds
+    Thread animThread;  // animation thread                       
+                                       
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        
+        // get the current window size     
+        Dimension dim = getSize();
+        xsize = dim.width;
+        ysize = dim.height; 
+
+        // clear background to white
+        g2.drawImage(background, 0, 0, null);
+        
+        //draw food
+        g2.setPaint(Color.green);
+        g2.fill(new Rectangle2D.Double(100, 100, xlen, ylen));
+        g2.setColor(Color.black);
+        g2.draw(new Rectangle2D.Double(100, 100, xlen, ylen));
+        
+        for(int i = 0; i<snakeSize; i++) {
+        	if(i==0) {
+        		// draw head
+        		g2.setPaint(Color.red);
+                g2.fill(new Rectangle2D.Double(xp[i], yp[i], xlen, ylen));
+                g2.setColor(Color.black);
+                g2.draw(new Rectangle2D.Double(xp[i], yp[i], xlen, ylen));
+        	}
+        	else {
+        		// draw body
+                g2.setPaint(Color.blue);
+                g2.fill(new Rectangle2D.Double(xp[i], yp[i], xlen, ylen));
+                g2.setColor(Color.black);
+                g2.draw(new Rectangle2D.Double(xp[i], yp[i], xlen, ylen));
+        	}	
+        }
+        
+
+        g2.dispose();
+    }
+    
+    // empty methods that are required by the GUI event loop
+    public void componentHidden (ComponentEvent e)  { }
+    public void componentMoved  (ComponentEvent e)  { }
+    public void componentResized(ComponentEvent e)  { }
+    public void componentShown  (ComponentEvent e)  { }
+    
+    public void checkWalls() {    
+       if (xp[0] + xlen >= xsize) { 
+           xp[0] = xsize - xlen;
+           dx   = 0;
+           dy	= -20;
+       } 
+       if (xp[0] < 0) {
+           xp[0] = 0;
+           dx   = 0;
+           dy	= 20;
+       } 
+       if (yp[0] + ylen >= ysize) {
+           yp[0] = ysize - ylen; 
+           dx   = 20;
+           dy 	= 0;
+       }             
+       if (yp[0] < 0) {
+           yp[0] = 0;
+           dx   = -20;
+           dy	= 0;
+       }
+    }
+    
+    public void checkFood() {
+    	if(xp[0]==100 && yp[0]==100) {
+    		snakeSize++;
+    	}
+    }
+    
+    public void run() {
+      while (true) {  // loop forever          
+        // update position
+        for(int i = snakeSize; i>0; i--) {
+        	xp[i] = xp[i-1];
+        	yp[i] = yp[i-1];
+        }
+    	xp[0] += dx;
+        yp[0] += dy;
+                
+        // check to see if the snake has hit any walls
+        /* not finished!!!!
+        checkWalls();
+        */
+        
+        checkFood();
+        
+        // sleep a bit until the next frame
+        try { Thread.sleep(delay); } 
+        catch (InterruptedException e) { 
+        	System.out.println("error");
+        	break;
+        }
+        
+        // refresh the display
+        repaint();  
+      }
+    }
+        
+    public SnakePanel() {
+        //loads background
+    	try {
+            background = ImageIO.read(new File("bg2.png"));
+        } catch (IOException e) {
+        }
+    	
+    	// set values for all the variables
+        xsize = 600;
+        ysize = 600;
+        xp[0]  = 300;
+        yp[0]  = 300;       
+        xlen  = 20;
+        ylen  = 20;
+        dx    = 0;
+        dy    = 20;
+        delay = 100;
+        for(int i = 1; i<snakeSize; i++) {
+        	xp[i] = xp[0]-(i*xlen*sign(dx));
+        	yp[i] = yp[0]-(i*ylen*sign(dy));
+        }
+        
+        // set up window properties
+        setBackground(Color.white);
+        setOpaque(true);
+        setPreferredSize(new Dimension((int) xsize, (int) ysize));
+        setFocusable(true);
+        addComponentListener(this);
+        
+        // start the animation thread
+        animThread = new Thread(this);
+        animThread.start();
+        
+        addKeyListener(this);
+    }
+    
+    private static int sign(float a) {
+    	if(a>0) {
+    		return 1;
+    	}
+    	else {
+    		if(a==0) {
+    			return 0;
+    		}
+    		else {
+    			return -1;
+    		}
+    	}
+    }
+
+	public void keyPressed(KeyEvent e) {
+		if(dx!=0) {
+			if(e.getKeyCode()==38) {
+				dy = -1*Math.abs(dx);
+				dx = 0;
+			}
+			if(e.getKeyCode()==40) {
+				dy = Math.abs(dx);
+				dx = 0;
+			}
+		}
+		else {
+			if(e.getKeyCode()==37) {
+				dx = -1*Math.abs(dy);
+				dy = 0;
+			}
+			if(e.getKeyCode()==39) {
+				dx = Math.abs(dy);
+				dy = 0;
+			}
+		}
+	}
+	
+	public void keyReleased(KeyEvent e) {}
+	public void keyTyped(KeyEvent e) {}
+}
