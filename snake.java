33a56d957de1504f043b455011ee9e00344998ed
Tomas <langer.tomas@yahoo.com>
2013-02-12 18:21:41 +0000

    Tomas
    deleting old stuff

--- snake.java
+++
@@ -1,8 +0,0 @@
-public class snake {
-
-    public static void main( String[] args ) {
-        System.out.println("Hello Guys, I just wanted to try it xD");
-        System.out.println("Also, Ill try to do something before the tutorial so we can be producitve xD");
-    }
-
-}
